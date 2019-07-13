ba78e2c813768cf1c4e877635cd1b13bca640e97
Christian Aschoff <christian.aschoff@diw-sc.de>
2019-07-13 10:03:01 +0200

    Write first class

---
+++ Code/Person.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Code
+{
+    public class Person
+    {
+        public string Vorname { get; }
+        public string Nachname { get; }
+        public int Alter { get; }
+        public double Gehalt { get; }
+        public string Standort { get; }
+
+        public Person(string vorname, string nachname, int alter, double gehalt, string standort)
+        {
+            this.Vorname = vorname;
+            this.Nachname = nachname;
+            this.Alter = alter;
+            this.Gehalt = gehalt;
+            this.Standort = standort;
+        }
+    }
+}
--- Code/Program.cs
+++ Code/Program.cs
@@ -6,7 +6,20 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person1 = new Person("Christian", "Aschoff", 41, 1250, "München");
+            Person person2 = new Person("David", "Reher", 30, 1320.30, "Dortmund");
+            Person person3 = new Person("Jörg", "Krampitz", 41, 1450.50, "Hannover");
+            Writeblock(person1);
+            Writeblock(person2);
+            Writeblock(person3);
+        }
+
+        private static void Writeblock(Person pers)
+        {
+            Console.WriteLine("Name: " + pers.Nachname + "," + pers.Vorname
+                              + "\t Alter:" + pers.Alter
+                              + "\t Gehalt:" + pers.Gehalt.ToString("###,###.00") + "€"
+                              + "\t Standort:" + pers.Standort);
         }
     }
 }
