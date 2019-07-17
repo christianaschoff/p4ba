66548ae41542a047001f16a5157080d4d71458e9
Christian Aschoff <CAschoff@BAGNB1206.local>
2019-07-17 21:05:29 +0200

    merge

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Model;
 using Contract;
 using Rules;
@@ -13,28 +13,26 @@ namespace Code
             IForecast forecast = new Forecast();
 
             IMitarbeiter mitarbeiter = new Mitarbeiter("Christian", "Aschoff", 41, 1250, "München", 7, ABTEILUNG.DEVELOPMENT, LEVEL.SENIOR);
-
-            IProjekt projekt = new Projekt("super duper");
-            projekt.Mitarbeiter.Add(mitarbeiter);
-
-            string[] liste = new string[10];
-            for(int i=0; i < liste.Length; i++) 
-            {
-                liste[i] = new String('A', i+1);
-            }
-
-            List<string> listeGeneric = new List<string>();
-            for(int i = 0; i < 10; i++) 
-            {
-                listeGeneric.Add(new String('A', i+1));
-            }
+            double gehalt = kostenrechner.BerechneJahresgehalt(mitarbeiter);
             
-            foreach(string element in listeGeneric)
-            {
-                Console.WriteLine(element);
+            Console.WriteLine(mitarbeiter.Vorname + " " + mitarbeiter.Nachname + " Grundgehalt: " + gehalt.ToString("#,###.00 €"));
+            
+            for(int i=1; i <= 5; i++) {
+                gehalt = forecast.Gehaltsentwicklung(gehalt);
+                 Console.WriteLine("Gehaltsentwicklung von " 
+                                    + mitarbeiter.Vorname + " " 
+                                    + mitarbeiter.Nachname + " " 
+                                    + " im " + i + ". Jahr: "
+                                    + gehalt.ToString("#,###.00 €"));
             }
+
+            Auslastung auslastung = new Auslastung();
+            for(int i = 210; i >= 0; i-=60)
+            {
+                Console.WriteLine("Auslastung ist bei " + i + " Tage(n) " + auslastung.BerechneAuslastungsstatus(i));
+            }
+           
         }
         
     }
 }
-
